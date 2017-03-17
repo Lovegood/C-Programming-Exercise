bce3af97544504cc08024e39191d56192df8c049
Cytosine <1316264211@qq.com>
2017-03-17 21:05:31 +0800

    3.5

---
+++ XUPT3.5/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double num1,num2,num3,num4,result;
+
+    printf("Please enter four numbers and separated by spaces.\n");
+    scanf("%lf %lf %lf %lf",&num1,&num2,&num3,&num4);
+
+    result=(num1+num2+num3+num4)/4.0;
+
+    printf("The average of the four numbers is %lf",result);
+
+    return 0;
+}
