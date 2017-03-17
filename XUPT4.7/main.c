02a07f59fac69bc68c4c56a8b5ff4eaa68410125
Cytosine <1316264211@qq.com>
2017-03-17 23:25:26 +0800

    4.7

--- XUPT4.7/main.c
+++ XUPT4.7/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int x;
+
+    printf("请输入一个正整数：");
+    scanf("%d",&x);
+
+    if(x>0&&x<32767){
+        int ge=x%10;
+
+        int length=1;
+        for(;x>10;length++){
+            x/=10;
+        }
+
+        printf("您输入正整数位数为%d",length);
+    }
+
     return 0;
 }
