cca7f39e7be675e68806e74b16e32ce0bb174b00
Cytosine <1316264211@qq.com>
2017-04-12 22:53:17 +0800

    4.4 finished

--- XUPT2 4.4/main.c
+++ XUPT2 4.4/main.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int sum;
+    for(int i=1;i<=100;i++){
+        sum=0;
+        for(int j=1;j<i;j++){//完数因子不计本身
+            if(i%j==0){
+                sum+=j;
+            }
+        }
+        if(sum==i){
+            printf("%d ",i);
+        }
+    }
     return 0;
 }
