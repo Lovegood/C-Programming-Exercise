717a8c80d0fde3f9a0d4806dcda121adc52704b2
Cytosine <1316264211@qq.com>
2017-04-12 22:46:00 +0800

    4.3 finished

--- XUPT2 4.3/main.c
+++ XUPT2 4.3/main.c
@@ -3,6 +3,16 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int num[3];
+    for(int i=100;i<=500;i++){
+        int cpy=i;
+        for(int j=0;j<3;j++){
+            num[j]=cpy%10;
+            cpy/=10;
+        }
+        if(i==pow(num[0],3)+pow(num[1],3)+pow(num[2],3)){
+            printf("%d ",i);
+        }
+    }
     return 0;
 }
