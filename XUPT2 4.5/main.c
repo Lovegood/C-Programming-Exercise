cfae357b05a2169ccd11e771fac708871c85a06b
Cytosine <1316264211@qq.com>
2017-04-12 23:11:06 +0800

    4.5

--- XUPT2 4.5/main.c
+++ XUPT2 4.5/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    int min,max,yu;
+    printf("请输入两个正整数，并以空格分隔它们：\n");
+    scanf("%d %d",&min,&max);
+    if(min>max){
+        int t=min;
+        min=max;
+        max=t;
+    }
+    //辗转相除法
+    while((yu=max%min)!=0){
+        max=min;
+        min=(yu>min)?min:yu;
+    }
+    printf("最大公约数为%d",min);
+
     return 0;
 }
