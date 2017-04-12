6b84de776411997723e3727bf987ec90e9b25354
Cytosine <1316264211@qq.com>
2017-04-12 23:25:53 +0800

    4.6 finished

--- XUPT2 4.6/main.c
+++ XUPT2 4.6/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
-    printf("Hello world!\n");
+    for(int n=1;n<=36;n++){
+        int num[7]={n};
+        for(int count=1;count<8;count++){
+            num[count]=num[count-1]+count;
+        }
+        int sum=0;
+        for(int i=0;i<7;i++){
+            sum+=num[i];
+        }
+        if(sum==105){
+            for(int i=0;i<7;i++){
+                printf("%d ",num[i]);
+            }
+        }
+    }
+
     return 0;
 }
