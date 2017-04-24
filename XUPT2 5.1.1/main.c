deb9a7da99da5bc92bff22d303965fce85b7203c
Cytosine <1316264211@qq.com>
2017-04-24 17:29:57 +0800

    5.1.1 finished

--- XUPT2 5.1.1/main.c
+++ XUPT2 5.1.1/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define r 3
+#define c 5
 
 int main()
 {
-    printf("Hello world!\n");
+    int a[r][c]={ {11,12,13,14,15} , {21,22,23,24,25} , {31,32,33,34,35} };
+    int * p[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            p[i][j]=&a[i][j];
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            printf("%p\t",p[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
