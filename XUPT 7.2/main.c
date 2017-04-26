bcf2742b1c392b22b148c30fe77fde3ba69e4bc8
Cytosine <1316264211@qq.com>
2017-04-26 17:32:40 +0800

    7.2 finished

--- XUPT 7.2/main.c
+++ XUPT 7.2/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define len 10
 
 int main()
 {
-    printf("Hello world!\n");
+    float score[len];
+    for(int i=0;i<len;i++){
+        printf("请输入第%d名学生的成绩:",i+1);
+        scanf("%f",&score[i]);
+    }
+
+    float max=score[0];
+    int index=0;
+    for(int i=1;i<len;i++){
+        if(score[i]>max){
+            max=score[i];
+            index=i;
+        }
+    }
+
+    printf("成绩最高这是第%d名同学，他的成绩为%f",index+1,max);
+
     return 0;
 }
