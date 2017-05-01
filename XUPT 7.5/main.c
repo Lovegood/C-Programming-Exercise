306e184f43c2a08fc718299956e065245b3e426c
Cytosine <1316264211@qq.com>
2017-05-01 17:13:56 +0800

    保存一个 去吃饭饭

--- XUPT 7.5/main.c
+++ XUPT 7.5/main.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define len sizeof(a)/sizeof(a[0])
 
 int main()
 {
     //初始化有序数列
     int * a;
     a=(int*)malloc(3*sizeof(int));
+    if(a==NULL){
+        printf("内存申请失败。");
+        exit(1);
+    }
     a[0]=2;
     a[1]=4;
     a[2]=7;
 
-    int input;
+    int input,index;
     printf("请输入欲插入数组的整数（输入q停止）:");
     while( scanf("%d",&input)==1 ){
-        a+=(int*)malloc
-
-        for(int i=0;i<sizeof(a)/sizeof(a[0]);i++){
-
+        index=0;
+        //重新申请内存
+        p=(int*)realloc(p,(len+1)*sizeof(int));
+        if(a==NULL){
+            printf("内存申请失败。");
+            exit(1);
+        }
+        //倒叙查找位置
+        for(int i=len-2;i>=0;i++){
+            if(a<a[i]){
+                ;
+            }else{
+            }
         }
 
         printf("请输入欲插入数组的整数（输入q停止）:");
