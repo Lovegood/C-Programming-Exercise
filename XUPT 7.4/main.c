9f5d38552dff12d56929c5e61af269de0f75dc7d
Cytosine <1316264211@qq.com>
2017-05-01 16:34:55 +0800

    7.4 调换未完成

--- XUPT 7.4/main.c
+++ XUPT 7.4/main.c
@@ -3,6 +3,32 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int len;
+    printf("请输入数组中元素个数：");
+    scanf("%d",&len);
+
+    int * a=(int*)malloc(len*sizeof(int));
+
+    for(int i=0;i<len;i++){
+        printf("请输入第%d个元素",i+1);
+        scanf("%d",&a[i]);
+    }
+
+    printf("数组内全部元素为：\n");
+    for(int i=0;i<len;i++){
+        printf("%d\t",a[i]);
+    }
+    printf("\n");
+
+    //change
+
+    printf("调换顺序后的数组元素为：\n");
+    for(int i=0;i<len;i++){
+        printf("%d\t",a[i]);
+    }
+    printf("\n");
+
+    free(a);
+
     return 0;
 }
--- XUPT2 5.1.2/main.c
+++ XUPT2 5.1.2/main.c
@@ -1,67 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int i,j;
-    int row,column;
-    printf("请输入欲输入的二维数组的行数：");
-    scanf("%d",&row);
-    printf("请输入欲输入的二维数组的列数：");
-    scanf("%d",&column);
+int main(){
+	int row,column,i,j,cnt;
+	int **array;
+	int index,max,isExisted=1;
 
-    //malloc
-    int ** a;
-    a=(int**)malloc(column*sizeof(int*));
-    if(NULL==a){
-        printf("error:内存申请失败");
-        exit(0);
-    }
-    for(i=0;i<row;i++){
-        a[i]=(int*)malloc(sizeof(int));
-        if(NULL==a[i]){
-            printf("error:内存申请失败");
-            exit(0);
-        }
-    }
+	printf("row:");
+	scanf("%d",&row);
+	printf("column:");
+	scanf("%d",&column);
 
-    //get input
-    for(i=0;i<row;i++){
-        for(j=0;j<column;j++){
-            printf("请输入第%d行，第%d列的数字：",i+1,j+1);
-            scanf("%d",a[i][j]);
-        }
-    }
-    //find
-    int p_c,isExisted=1;
-    int max;
-    //查找每行最大，判断是否为该列最小
-    for(i=0;i<row;i++){
-        max=a[i][0];
-        p_c=0;
-        for(j=1;j<column;j++){
-            if(a[i][j]>max){
-                max=a[i][j];
-                p_c=j;
-            }
-        }
-        for(int cnt=0;cnt<row;cnt++){
-            if(a[i][j]>=a[cnt][j]){
-                isExisted=0;
-                break;
-            }
-        }
-    }
-    if(0==isExisted){
-        printf("该二维数组不存在鞍点。\n");
-    }else{
-        printf("该二维数组的鞍点为%d",a[i][j]);
-    }
-    //free
-    for(i=0;i<row;i++){
-        free(a[i]);
-    }
-    free(a);
+	array=(int**)malloc(row*sizeof(int*));
+	for(i=0;i<row;i++){
+		array[i]=(int*)malloc(column*sizeof(int));
+	}
 
-    return 0;
+	for(i=0;i<row;i++){
+		for(j=0;j<column;j++){
+			printf("%d,%d:",i,j);
+			scanf("%d",&array[i][j]);
+		}
+	}
+
+	for(i=0;i<row;i++){
+		for(j=0;j<column;j++){
+			printf("%d\t",array[i][j]);
+		}
+		printf("\n");
+	}
+
+	//find
+	for(i=0;i<row;i++){
+		max=array[i][0];
+		index=0;
+		for(j=1;j<column;j++){//max in row
+			if(array[i][j]>max){
+				max=array[i][j];
+				index=j;
+			}
+		}
+		for(cnt=0;cnt<row;cnt++){//max is min in column?
+			if(array[cnt][index]<=max){
+				isExisted=0;
+				break;
+			}
+		}
+		if(isExisted==1){
+			printf("鞍点是%d",max);
+		}
+	}
+	if(isExisted==0){
+		printf("鞍点不存在\n");
+	}
+
+	for(i=0;i<row;i++){
+		free(array[i]);
+	}
+	free(array);
+
+	return 0;
 }
