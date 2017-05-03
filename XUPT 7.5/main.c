e48e8a0abbc8c2877d346b9724f467e0e3caf723
Cytosine <1316264211@qq.com>
2017-05-03 18:04:01 +0800

    7.5 finished

--- XUPT 7.5/main.c
+++ XUPT 7.5/main.c
@@ -1,38 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define len sizeof(a)/sizeof(a[0])
 
 int main()
 {
     //初始化有序数列
-    int * a;
-    a=(int*)malloc(3*sizeof(int));
+    int * a=(int*)malloc(3*sizeof(int));
     if(a==NULL){
         printf("内存申请失败。");
         exit(1);
     }
     a[0]=2;
     a[1]=4;
-    a[2]=7;
+    a[2]=6;
 
-    int input,index;
+    int cnt=3;
+
+    printf("当前数组：\n");
+    for(int i=0;i<cnt;i++){
+        printf("%d\t",a[i]);
+    }
+    printf("\n");
+
+    int input;
     printf("请输入欲插入数组的整数（输入q停止）:");
     while( scanf("%d",&input)==1 ){
-        index=0;
         //重新申请内存
-        p=(int*)realloc(p,(len+1)*sizeof(int));
+        a=(int*)realloc(a,(cnt+1)*sizeof(int));
         if(a==NULL){
             printf("内存申请失败。");
             exit(1);
         }
-        //倒叙查找位置
-        for(int i=len-2;i>=0;i++){
-            if(a<a[i]){
-                ;
-            }else{
-            }
-        }
+        cnt++;
+        //倒叙查找位置并插入
+        for(int i=cnt-2;i>=0;i--){
+            if(input<a[i]){
+                a[i+1]=a[i];
+                if(i==0){
+                    a[i]=input;
 
+                    printf("当前数组：\n");
+                    for(int i=0;i<cnt;i++){
+                        printf("%d\t",a[i]);
+                    }
+                    printf("\n");
+                }
+            }else{
+                a[i+1]=input;
+
+                printf("当前数组：\n");
+                for(int i=0;i<cnt;i++){
+                    printf("%d\t",a[i]);
+                }
+                printf("\n");
+
+                break;
+            }
+
+        }
         printf("请输入欲插入数组的整数（输入q停止）:");
     }
 
