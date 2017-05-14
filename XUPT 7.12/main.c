4dd0897bb62423f0389bd3ae3700e460ee0fe63c
Cytosine <1316264211@qq.com>
2017-05-14 22:56:49 +0800

    7.12 finished

--- XUPT 7.12/main.c
+++ XUPT 7.12/main.c
@@ -1,38 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #define max 101
 
-//用二维字符数组的每行存储键盘输入的字符串，将这些字符串按字典顺序升序排序，按排序后的结果输出。
+int main(){
+    //input
+    int size;
+    printf("每行字符数不超高100，请输入欲输入的行数：");
+    scanf("%d",&size);
 
-int main()
-{
-    char ** a=(char**)malloc(sizeof(char*));
-    //输入
-    printf("请根据提示输入每行内容，每行内容不超过100个字符，按q停止输入。\n");
-    int line=0;
-    for(;;line++){
-        printf("请输入第%d行内容：",line+1);
-        scanf("%s",&a[line]);
-        if(strcmp(a[line],"q")==0){
-            free(a[line]);
-            break;
-        }
-        a=(char**)realloc(a,(line+1)*sizeof(char*));
-        a[line]=(char*)malloc(max*sizeof(char));
+    char a[size][max];
+    for(int i=0;i<size;i++){
+        printf("请输入第%d行内容：",i+1);
+        scanf("%s",&a[i]);
     }
 
-    printf("字符串列表：\n");
-    for(int i=0;i<line;i++){
+    printf("当前字符串列表内容为：\n");
+    for(int i=0;i<size;i++){
         printf("%s\n",a[i]);
     }
-    //排序
 
-    //释放
-    for(int i=0;i<line;i--){
-        free(a[i]);
+    //list
+    char t[max];
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size-i-1;j++){
+            if( strcmp(a[j],a[j+1])>0 ){
+                strcpy(t,a[j]);
+                strcpy(a[j],a[j+1]);
+                strcpy(a[j+1],t);
+            }
+        }
     }
-    free(a);
+
+    //output
+    printf("排序后字符串列表内容为：\n");
+    for(int i=0;i<size;i++){
+        printf("%s\n",a[i]);
+    }
 
     return 0;
 }
