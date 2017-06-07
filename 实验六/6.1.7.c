83cfcf565b1dac2091a87fabd207a1503797cd42
Cytosine <Lovegood@users.noreply.github.com>
2017-06-07 02:05:23 -0500

    Update 6.1.7.c

--- 实验六/6.1.7.c
+++ 实验六/6.1.7.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-int cntAge(int);
+int cntAge(int,int);
 
 int main(){
 	int n;
 	printf("请输入n的值：",&n);
 	scanf("%d",&n);
-	printf("第%d个人的年龄为%d",n,cntAge(n));
+	printf("第%d个人的年龄为%d",n,cntAge(n,10));
 	
 	return 0;
 }
 
-int cntAge(int n){
+int cntAge(int n,int age){
 	if(n>0){
-		age=cntAge(n-1);
+		age=cntAge(n-1,age)+2;
 	}
 	return 
+age;
 }
